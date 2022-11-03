8ec5e6e504edf345f51e00f3dd261bb0cf49dfb4
Retro <108846245+retroDOTexe@users.noreply.github.com>
2022-11-03 09:51:08 -0400

    work pls
    fingers crossed
    
    Signed-off-by: Retro <108846245+retroDOTexe@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 (function() {
+    // Add event listener
     document.addEventListener("mousemove", parallax);
     const elem = document.querySelector("#parallax");
+    // Magic happens here
     function parallax(e) {
         let _w = window.innerWidth/2;
         let _h = window.innerHeight/2;
